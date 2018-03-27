7c06e7d5293314f8c806bc8e21ed39d0ab3831e1
user <user@PC>
2018-03-27 23:03:29 +0600

    Add project files.

---
+++ HackingTests/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using  Microsoft.Office.Interop.Excel;
+namespace HackingTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Process myTestProcess = Process.GetProcesses().FirstOrDefault(w => w.ProcessName.Contains("MyTestStudent"));
+            //AppDomain dom = AppDomain.CurrentDomain;
+            
+            //foreach (Assembly assembly in dom.GetAssemblies())
+            //{
+            //    Console.WriteLine(assembly.CodeBase);
+            //}
+
+            //ProcessStartInfo processsrInfo = new ProcessStartInfo("MyTestEditor.exe");
+            //Process s= Process.Start(processsrInfo);
+            //processsrInfo.Arguments = "SomeTest.mtx";
+            //Console.WriteLine(s.Start());
+           
+            
+            Application excelApp = new Application();
+            Workbook wpWorkbook = excelApp.Workbooks._Open(@"C:\Users\user\Desktop\123.xlsx");
+            Worksheet worksheet = wpWorkbook.Worksheets[1];
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackingTests_Interface/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace HackingTests_Interface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string _executionFileName;
+        private string _testFileName;
+        private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog injectorFileDialog = new OpenFileDialog
+            {
+                Title = "Выберите файл",
+                Filter = "exe файлы|*.exe"
+            };
+            bool? showDialog = injectorFileDialog.ShowDialog();
+            if (showDialog != null && (bool)showDialog)
+            {
+                _executionFileName = injectorFileDialog.FileName;
+                MessageTextBlock.Text += "1) Инжекторный файл был успешно добавлен!\n";
+            }
+        }
+
+        private void SearchFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            switch (string.IsNullOrEmpty(_executionFileName) && string.IsNullOrEmpty(_testFileName))
+            {
+                case false:
+                    {
+                        try
+                        {
+                            StartHacking(_executionFileName, _testFileName);
+                        }
+                        catch (Exception ex)
+                        {
+                            ErrorOrSuccesTextBlock.Text += ex;
+                        }
+                        break;
+                    }
+            }
+        }
+
+
+        private void StartHacking(string injectionFilename, string sourceFileName)
+        {
+            ProcessStartInfo process = new ProcessStartInfo(injectionFilename)
+            { Arguments = sourceFileName };
+            Process s = Process.Start(process);
+            Console.WriteLine(s != null && s.Start());
+        }
+        private void SourceMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog sourceDialog = new OpenFileDialog()
+            {
+                Title = "Выберите файл",
+                Filter = "Файлы формата mtx|*.mtx"
+            };
+            bool? showDialog = sourceDialog.ShowDialog();
+            if (showDialog != null && (bool)showDialog)
+            {
+                _testFileName = sourceDialog.FileName;
+                MessageTextBlock.Text += "2) Исходный файл был успешно добавлен!";
+            }
+        }
+    }
+}
